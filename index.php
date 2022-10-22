53c57227afa45d3034e344d1da199d6debd91493
Marwan1998 <Dragonkingofhell@gmail.com>
2022-10-22 19:08:56 +0200

    re added supplier classes and the index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+this index file to testing classes.
+*/
+include_once 'includes/classes/Supplier.php';
+
+echo "<h1>Hello World!</h1>";
+
+// $s1 = new Supplier();
+// $supplier = $s1->getSupplier(23);
+
+// print_r($supplier);
