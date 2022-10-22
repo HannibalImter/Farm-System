580780654613e6c00c518ad84246defa3b04db5a
Marwan1998 <Dragonkingofhell@gmail.com>
2022-10-22 15:56:31 +0200

    added suppliers class

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'includes/classes/Supplier.php';
+
+echo "<h1>Hello World</h1>";
+//Index to test the classes.
+
+// $s1 = new Supplier('Name', 'phoneAsString');
+// $s1->insertNewSupplier();
+
+// $result = $s1->getAllSuppliers();
+
+// echo "<pre>";
+// print_r($result);
+// echo "</pre>";
