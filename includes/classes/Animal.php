4c1bf1b67a159899f55e5ae7bcbc75ee43d16ad3
Marwan1998 <Dragonkingofhell@gmail.com>
2022-10-23 16:02:25 +0200

    done with animal class

--- includes/classes/Animal.php
+++ includes/classes/Animal.php
@@ -5,5 +5,130 @@ include_once 'Database.php';
 */
 class Animal
 {
-    
+    private $id; //auto
+    private $animalCategoriesID;
+    private $start_date; //auto
+    private $endDate = null;
+    private $barnID;
+    private $quantity;
+    private $state;
+    private $supplierID;
+    private $price;
+    private $cost;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function setValues($animeCatID, $bID, $quan, $supID, $price, $cost, $state='')
+    {
+        $this->animalCategoriesID = $animeCatID;
+        $this->barnID = $bID;
+        $this->quantity = $quan;
+        $this->supplierID = $supID;
+        $this->price = $price;
+        $this->cost = $cost;
+        $this->state = $state;
+    }
+
+    public function insertNewAnimal()
+    {
+        $sql = "INSERT INTO animals (animal_categories_id, barn_id,
+         quantity, state, supplier_id , price, cost) VALUES
+          (:aCID, :bID, :quan, :state, :supID, :price, :cost)";
+
+        $values = array(
+            array(':aCID', $this->animalCategoriesID),
+            array(':bID', $this->barnID),
+            array(':quan', $this->quantity),
+            array(':state', $this->state),
+            array(':supID', $this->supplierID),
+            array(':price', $this->price),
+            array(':cost', $this->cost)
+        );
+
+        try {
+            $this->db->queryDB($sql, Database::EXECUTE, $values);
+            return true;
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to insert a new Animal \n";
+            throw $th;
+        }
+    }
+
+    public function deleteAnimal($id = -1)
+    {
+        $sql = "UPDATE animals SET deleted_at = NOW() WHERE id = $id";
+        try {
+            $this->db->queryDB($sql, Database::EXECUTE);
+            return true;
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to delete the Animal: \n";
+            throw $th;
+        }
+    }
+
+    public function deleteSupplierByState($state = '')
+    {
+        $sql = "UPDATE animals SET deleted_at = NOW() WHERE state = :state";
+        $values = array(
+            array(':state', $state)
+        );
+        try {
+            $this->db->queryDB($sql, Database::EXECUTE, $values);
+            return true;
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to delete the Animal: \n";
+            throw $th;
+        }
+    }
+
+    public function getAllAnimals()
+    {
+        $sql = "SELECT * FROM animals WHERE deleted_at is NULL";
+        try {
+            return $this->db->queryDB($sql, Database::SELECTALL);
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to get all animals: \n";
+            throw $th;
+        }
+    }
+
+    public function getAnimal($id = -1)
+    {
+        $sql = "SELECT * FROM animals WHERE id = $id AND deleted_at is NULL";
+        try {
+            return $this->db->queryDB($sql, Database::SELECTSINGLE);
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to get the Animal: \n";
+            throw $th;
+        }
+    }
+
+    public function doQuery($sql, $mode, $valuesToBind = array())
+    {
+        //A function to do a specific  query.
+        try {
+            return $this->db->queryDB($sql, $mode, $valuesToBind);
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to go execute the query: \n";
+            throw $th;
+        }
+    }
+
+    public function increaseQuantity($animalID = -1, $increaseBy = 1)
+    {
+        $sql = "UPDATE animals SET quantity = quantity + $increaseBy WHERE id = $animalID AND deleted_at is NULL";
+        try {
+            $this->db->queryDB($sql, Database::EXECUTE);
+            return true;
+        } catch (\Throwable $th) {
+            echo "An error occurred while trying to increase the animal quantity \n";
+            throw $th;
+        }
+    }
+
+
+
 }
--- index.php
+++ index.php
@@ -3,10 +3,28 @@
 this index file to testing classes.
 */
 include_once 'includes/classes/Supplier.php';
+include_once 'includes/classes/Animal.php';
 
+function logout($data)
+{ //debuging tool, just ignore it.
+    if (is_array($data))
+        $data = implode(', ', $data);
+    echo "<script>console.log('Log-Out: " . $data . "' );</script>";
+}
 echo "<h1>Hello World!</h1>";
+// ---------------------------------------------------------------------------------
 
-// $s1 = new Supplier();
-// $supplier = $s1->getSupplier(23);
 
-// print_r($supplier);
+$animal = new Animal();
+
+// $animal->setValues(1, 1, 3, 23, 13.5, 7.25, 'active');
+// $animal->insertNewAnimal();
+
+// $animal->deleteSupplierByState('active');
+// $animal->increaseQuantity(animalID: 19, increaseBy: 10);
+
+$result = $animal->getAllAnimals();
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
