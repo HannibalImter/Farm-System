3d607930e7a12a67750b851f9b7354aa07d83226
alaref-codes <alarefabushaala@gmail.com>
2022-11-28 22:27:41 +0200

    Added the resources files

---
+++ resource.php
@@ -0,0 +1,59 @@
+<?php
+    include "UI_include.php";
+    include INC_DIR."/process/p-login.php";
+    include INC_DIR.'header.html';
+    include_once 'includes/classes/Resource.php';
+?>
+
+<header class="header">
+  <h1 class="logo"><a href="#">My Farm Mangment system</a></h1>
+  <ul class="main-nav">
+  </ul>
+</header>
+<body>
+
+    <div class="container my-5">
+        <h2>List Of Resources</h2>
+        <a href="includes/process/p-resource/p-add-resource.php" class="btn btn-primary" role="button">New Resource</a>
+        <br>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Resource ID</th>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $resources = new Resource();
+                $result = $resources->getAllResources();
+                if (!$result) {
+                    die("not found!");
+                }
+                foreach ($result as $key => $value) {
+                    echo "
+                        <tr>
+                        <td>$value[id]</td>
+                        <td>$value[name]</td>
+                        <td>$value[quantity]</td>
+                        <td>
+                            <a class='btn btn-primary btn-sm'
+                             href='includes/process/p-resource/p-edit.php?id=$value[id]'>Edit</a>
+                            <a class='btn btn-danger btn-sm'
+                              href='includes/process/p-resource/p-delete.php?id=$value[id]'>Delete</a>
+                        </td>
+                        </tr>
+                    ";
+                }
+
+                ?>
+                
+            </tbody>
+        </table>
+
+    </div>
+
+
+</body>
+</html>
